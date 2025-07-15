f176dd5ff91dcf1cf6ed9ac8beae8074a2fd2aa4
Lars276491 <laura.straccini@gmail.com>
2025-07-15 16:57:59 +0200

    modificati tutti gli html e aggiunti credenziali, user e recensioni

---
+++ src/main/java/it/uniroma3/siw/model/Credenziali.java
@@ -0,0 +1,69 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Credenziali {
+
+	public static final String DEFAULT_ROLE = "DEFAULT";
+	public static final String ADMIN_ROLE = "ADMIN";
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	private String username;
+	private String password;
+	private String role;
+
+	// RELAZIONE esplicita verso User, senza cascade persist
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
